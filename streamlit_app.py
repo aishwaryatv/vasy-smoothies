f8a254b170913918d67d8454a78accbe0fb34c9b
aishwaryatv <44039306+aishwaryatv@users.noreply.github.com>
2026-06-16 10:58:46 +0200

    Comment out dataframe display and stop calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 #Convert the snowflake dataframe to panda dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredient_list = st.multiselect('Choose upto to 5 ingredients : '
                                 ,my_dataframe,max_selections=5)
